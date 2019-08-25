f8491926e5bbc1d630dcab4ddeff68bb3672f80e
Adamorhenner <adacandido9@gmail.com>
2019-08-25 01:12:33 -0300

    Traduções .py para js

--- exercicio12.py
+++
@@ -1,5 +0,0 @@
-print("Avaliação se um numero eh maior que o outro")
-x = (int)(input("Digite o primeiro numero: "))
-y = (int)(input("Digite o segundo numero: "))
-expressao = x>=y
-print(expressao)
